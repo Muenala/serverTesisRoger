ed195fafa94882aa48213ba0cb3f6c712dc11226
kevin.muenala <kevin.muenala@educacion.gob.ec>
2023-02-22 00:41:06 -0500

    latest

--- app.js
+++ app.js
@@ -33,6 +33,6 @@ dbConnect();
 https.createServer({
   cert: fs.readFileSync('mi_certificado.crt'),
   key: fs.readFileSync('mi_certificado.key')
-},app).listen(PUERTO, function(){
+},app).listen(PORT, function(){
  console.log('Servidor https correindo en el puerto 443');
 });
