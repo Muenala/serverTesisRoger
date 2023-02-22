6a2277664a4e597ec9364890722c9a4947440da8
kevin.muenala <kevin.muenala@educacion.gob.ec>
2023-02-22 00:33:07 -0500

    latest

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ require('dotenv').config();
 const express = require('express');
 const { dbConnect } = require('./config/mongo')
 const cors = require('cors');
-
+var fs = require('fs');
+var https = require('https');
 const app = express();
-const PORT = process.env.PORT || 3000;
-const http = require('http').Server(app);
+const PORT = process.env.PORT || 443;
+
+
+/* const http = require('http').Server(app); */
 var fileupload = require("express-fileupload");
 app.use(fileupload());
 
@@ -24,6 +27,12 @@ app.use('/api/', require('./app/routes'))
 
 //connection mongodb
 dbConnect();
-http.listen(PORT, () => {
+/* http.listen(PORT, () => {
   console.log('nodejs app run ' + PORT)
-})
+}) */
+https.createServer({
+  cert: fs.readFileSync('mi_certificado.crt'),
+  key: fs.readFileSync('mi_certificado.key')
+},app).listen(PUERTO, function(){
+ console.log('Servidor https correindo en el puerto 443');
+});
